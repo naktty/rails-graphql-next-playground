71effa12e5dfb53052676f47bbdcb9084d988141
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-02-13 20:04:29 +0900

    HelloWorldコンポーネントを追加

---
+++ next/src/pages/hello_world.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+
+const HelloWorld: NextPage = () => {
+  return (
+    <>
+      <h1>Title</h1>
+      <p>content</p>
+    </>
+  )
+}
+
+export default HelloWorld
