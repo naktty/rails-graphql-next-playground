fccdb77dfe68fcd1f472d7934e258140145e7ed7
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-02-13 20:10:36 +0900

    SimpleButtonコンポーネントを追加し、HelloWorldページに組み込み

--- next/src/pages/hello_world.tsx
+++ next/src/pages/hello_world.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
+import SimpleButton from '@/components/SimpleButton'
 
 const HelloWorld: NextPage = () => {
   return (
     <>
       <h1>Title</h1>
       <p>content</p>
+      <SimpleButton />
     </>
   )
 }
