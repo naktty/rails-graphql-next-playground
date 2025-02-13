0167c21c8f295d868a7600f931d3097c250be7e4
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-02-13 20:16:20 +0900

    SimpleButtonコンポーネントにテキストプロパティを追加し、HelloWorldページで使用

--- next/src/components/SimpleButton.tsx
+++ next/src/components/SimpleButton.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const SimpleButton: React.FC = () => {
-  const handleClick = () => {
-    console.log('Button clicked')
+type SimpleButtonProps = {
+  text: string
+}
+
+const SimpleButton: React.FC<SimpleButtonProps> = (props) => {
+  const handleOnClick = () => {
+    console.log('Clicked!')
   }
 
-  return <button onClick={handleClick}>Click me</button>
+  return <button onClick={handleOnClick}>{props.text}</button>
 }
 
 export default SimpleButton
