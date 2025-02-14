be4aa0bf485a92e64e2f88b62359999d8a8da863
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-02-14 09:33:15 +0900

    rubocop

--- rails/app/graphql/types/photo_category_type.rb
+++ rails/app/graphql/types/photo_category_type.rb
@@ -2,12 +2,12 @@
 
 module Types
   class PhotoCategoryType < Types::BaseEnum
-    description 'Category of the photo'
+    description "Category of the photo"
 
-    value 'SELFIE', 'A photo taken of oneself', value: :selfie
-    value 'PORTRAIT', 'A portrait photo', value: :portrait
-    value 'ACTION', 'An action photo', value: :action
-    value 'LANDSCAPE', 'A landscape photo', value: :landscape
-    value 'GRAPHIC', 'A graphic photo', value: :graphic
+    value "SELFIE", "A photo taken of oneself", value: :selfie
+    value "PORTRAIT", "A portrait photo", value: :portrait
+    value "ACTION", "An action photo", value: :action
+    value "LANDSCAPE", "A landscape photo", value: :landscape
+    value "GRAPHIC", "A graphic photo", value: :graphic
   end
 end
