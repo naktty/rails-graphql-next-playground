9c32f1076596ced32d6af3ab11d4bffe2dafedfb
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-02-14 18:31:02 +0900

    PhotoCategoryType列挙型の値を更新し、文字列リテラルを使用

--- rails/app/graphql/types/photo_category_type.rb
+++ rails/app/graphql/types/photo_category_type.rb
@@ -4,10 +4,10 @@ module Types
   class PhotoCategoryType < Types::BaseEnum
     description "Category of the photo"
 
-    value "SELFIE", "A photo taken of oneself", value: :selfie
-    value "PORTRAIT", "A portrait photo", value: :portrait
-    value "ACTION", "An action photo", value: :action
-    value "LANDSCAPE", "A landscape photo", value: :landscape
-    value "GRAPHIC", "A graphic photo", value: :graphic
+    value "SELFIE", "A photo taken of oneself", value: 'selfie'
+    value "PORTRAIT", "A portrait photo", value: 'portrait'
+    value "ACTION", "An action photo", value: 'action'
+    value "LANDSCAPE", "A landscape photo", value: 'landscape'
+    value "GRAPHIC", "A graphic photo", value: 'graphic'
   end
 end
