b297d974450cacf23456bc180501a6dc8f265a84
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-02-14 17:47:26 +0900

    開発中のアセットパイプラインとGraphiQL用にRailsを設定する
    - Railsサーバを実行するようにdocker-compose.ymlを更新する
    - sprockets-rails gemを追加
    - アセット設定用のmanifest.jsを作成
    - application.rbでsprocketsとセッションミドルウェアを有効にする
    - 開発環境用にGraphiQLルートを追加

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -11,6 +11,7 @@ require "action_mailer/railtie"
 require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
+require "sprockets/railtie"
 # require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
@@ -44,5 +45,7 @@ module Myapp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
