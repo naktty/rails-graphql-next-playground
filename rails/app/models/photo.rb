21f5e9ebcfc68e32a5996c665a06b7e1d0c1ab28
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-02-15 15:27:59 +0900

    rubocop

--- rails/app/models/photo.rb
+++ rails/app/models/photo.rb
@@ -5,6 +5,6 @@ class Photo < ApplicationRecord
 
   validates :name, presence: true
   validates :url, presence: true
-  
+
   enum category: { selfie: 0, portrait: 1, action: 2, landscape: 3, graphic: 4 }
 end
