4a6b26c310283f0b7c162ffe727afaa68422878a
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-02-15 15:06:59 +0900

    ユーザーと写真のタグ付けのためのPhotoTagモデルと関連付けの追加
    - PhotoTagモデルを作成して、ユーザと写真の間に多対多のリレーションシップを確立する
    - Photo、User、PhotoTagモデルに関連付けを追加する
    - photo_idとuser_idに一意なインデックスを持つphoto_tagsテーブルのマイグレーションを作成する
    - photo_tagsテーブルと外部キー制約を含むようにスキーマを更新

--- rails/app/models/photo.rb
+++ rails/app/models/photo.rb
@@ -1,6 +1,10 @@
 class Photo < ApplicationRecord
   belongs_to :user
+  has_many :photo_tags, dependent: :destroy
+  has_many :tagged_users, through: :photo_tags, source: :user
+
   validates :name, presence: true
   validates :url, presence: true
+  
   enum category: { selfie: 0, portrait: 1, action: 2, landscape: 3, graphic: 4 }
 end
--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :photos, dependent: :destroy
+  has_many :photo_tags, dependent: :destroy
+  has_many :tagged_photos, through: :photo_tags, source: :photo
+
   validates :github_login, presence: true, uniqueness: true
 end
