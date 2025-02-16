c3c76bda97d5983a3916ae7dafcc0938cedbf27e
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-02-16 11:02:33 +0900

    rubocop

--- rails/app/graphql/loaders/association_loader.rb
+++ rails/app/graphql/loaders/association_loader.rb
@@ -15,6 +15,7 @@ module Loaders
     def load(record)
       raise TypeError, "#{@model} loader can't load association for #{record.class}" unless record.is_a?(@model)
       return Promise.resolve(read_association(record)) if association_loaded?(record)
+
       super
     end
 
@@ -25,27 +26,27 @@ module Loaders
 
     def perform(records)
       preload_association(records)
-      records.each { |record| fulfill(record, read_association(record)) }
+      records.each {|record| fulfill(record, read_association(record)) }
     end
 
     private
 
-    def validate
-      unless @model.reflect_on_association(@association_name)
-        raise ArgumentError, "No association #{@association_name} on #{@model}"
+      def validate
+        unless @model.reflect_on_association(@association_name)
+          raise ArgumentError, "No association #{@association_name} on #{@model}"
+        end
       end
-    end
 
-    def preload_association(records)
-      ::ActiveRecord::Associations::Preloader.new(records: records, associations: @association_name).call
-    end
+      def preload_association(records)
+        ::ActiveRecord::Associations::Preloader.new(records: records, associations: @association_name).call
+      end
 
-    def read_association(record)
-      record.public_send(@association_name)
-    end
+      def read_association(record)
+        record.public_send(@association_name)
+      end
 
-    def association_loaded?(record)
-      record.association(@association_name).loaded?
-    end
+      def association_loaded?(record)
+        record.association(@association_name).loaded?
+      end
   end
 end
