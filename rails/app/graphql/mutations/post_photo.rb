053386c921c3b3f37bfc333e9cb54e206e694787
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-02-22 22:58:36 +0900

    写真投稿ロジックの改善
    - デフォルト値を持つオプションのカテゴリー引数を追加
    - フォトIDに基づいてフォトURLを動的に生成

--- rails/app/graphql/mutations/post_photo.rb
+++ rails/app/graphql/mutations/post_photo.rb
@@ -1,17 +1,14 @@
-# frozen_string_literal: true
-
 module Mutations
   class PostPhoto < BaseMutation
     field :photo, Types::PhotoType, null: true
 
     argument :name, String, required: true
     argument :description, String, required: false
+    argument :category, Types::PhotoCategory, required: false, default_value: 'portrait'
 
     def resolve(**args)
-      photo_num = 30
-      photo_num += 1
-      photo_url = "https://example.com/photos/#{photo_num}.jpg"
-      photo = Photo.create!(**args.merge(user: User.first, url: photo_url))
+      photo = Photo.create!(**args.merge(user: User.first,url: "pending"))
+      photo.update!(url: "https://example.com/photos/#{photo.id}.jpg")
       {
         photo: photo,
       }
