344600f229121123cd1244d5a7ac4e8e2779853c
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-02-23 22:43:39 +0900

    FakeUserAuth GraphQL ミューテーションを追加
    - 開発環境でのユーザー認証をサポートするためのGraphQL Mutationを作成
    - GitHub ログイン名を使用してユーザーを認証する
    - MutationTypeに新しいミューテーションを登録

--- rails/app/graphql/types/mutation_type.rb
+++ rails/app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@
 
 module Types
   class MutationType < Types::BaseObject
+    field :fake_user_auth, mutation: Mutations::FakeUserAuth
     field :add_fake_users, mutation: Mutations::AddFakeUsers
     field :github_auth, mutation: Mutations::GithubAuth
     field :post_photo, mutation: Mutations::PostPhoto
