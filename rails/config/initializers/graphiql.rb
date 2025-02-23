2003edcb31d982faea7923e4464dab707ccd0637
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-02-23 20:37:01 +0900

    開発環境用にGraphiQLの設定を更新

--- rails/config/initializers/graphiql.rb
+++ rails/config/initializers/graphiql.rb
@@ -1,5 +1,7 @@
-GraphiQL::Rails.config.headers = {
-  "Authorization" => ->(_context) {
-    "Bearer #{ENV["TMP_TOKEN"]}"
-  },
-}
+if defined?(GraphiQL::Rails) && Rails.env.development?
+  GraphiQL::Rails.config.headers = {
+    "Authorization" => ->(_context) {
+      "Bearer #{ENV["TMP_TOKEN"]}"
+    }
+  }
+end
