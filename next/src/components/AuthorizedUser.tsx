9e906046047ededcf1c7f6c444a51b966b27647c
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-02-27 23:06:19 +0900

    GitHub OAuth 認証フローの実装
    - AuthorizedUserコンポーネントにGitHub OAuth認証のmutationを追加する
    - Next.jsの設定を更新してGitHubのアバター画像を許可するようにする
    - ログイン処理が二重実行されるのでReactのストリクトモードを無効にする

--- next/src/components/AuthorizedUser.tsx
+++ next/src/components/AuthorizedUser.tsx
@@ -1,19 +1,40 @@
+import { gql, useMutation } from '@apollo/client'
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
+import { ROOT_QUERY } from '../pages'
+
+const GITHUB_AUTH_MUTATION = gql`
+  mutation githubAuth($input: GithubAuthInput!) {
+    githubAuth(input: $input) {
+      authPayload {
+        token
+      }
+    }
+  }
+`
 
 const AuthorizedUser: NextPage = () => {
   const [signingIn, setSigningIn] = useState(false)
   const router = useRouter()
 
+  const authorizationComplete = () => {
+    router.replace('/')
+    setSigningIn(false)
+  }
+
+  const [githubAuth] = useMutation(GITHUB_AUTH_MUTATION, {
+    update: authorizationComplete,
+    refetchQueries: [{ query: ROOT_QUERY }],
+  })
+
   useEffect(() => {
     if (window.location.search.match(/code=/)) {
       setSigningIn(true)
       const code = window.location.search.replace('?code=', '')
-      alert(code)
-      router.replace('/')
+      githubAuth({ variables: { input: { code } } })
     }
-  }, [router])
+  }, [githubAuth, router])
 
   const requestCode = (): void => {
     const clientId = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID
--- rails/app/controllers/graphql_controller.rb
+++ rails/app/controllers/graphql_controller.rb
@@ -11,7 +11,7 @@ class GraphqlController < ApplicationController
     query = params[:query]
     operation_name = params[:operationName]
     context = {
-      current_user: current_user,
+      current_user: current_user
     }
     result = MyappSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
@@ -51,10 +51,10 @@ class GraphqlController < ApplicationController
     end
 
     def current_user
-      # token = request.headers["Authorization"].split(" ")&.last
-      # return nil unless token
+      token = request.headers["Authorization"].present? ? request.headers["Authorization"].split(" ")&.last : nil
+      return nil unless token
 
-      token = ENV["TMP_TOKEN"]
+      # token = ENV["TMP_TOKEN"]
       User.find_by(github_token: token)
     end
 end
