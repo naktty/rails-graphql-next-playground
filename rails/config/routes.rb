d6b48e745551b50a92b3b85eadd2bba810a24b02
nakaty <78027524+nakaty2023@users.noreply.github.com>
2025-03-01 21:19:51 +0900

    Action Cable用のルーティングを追加
    - ルーティングファイルにAction Cableのエンドポイントを設定
    - WebSocketサーバーを"/cable"パスでマウント

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
 
+  mount ActionCable.server => "/cable"
+
   namespace :api do
     namespace :v1 do
       get "health_check", to: "health_check#index"
